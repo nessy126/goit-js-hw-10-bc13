929e62f1e5fd378a91c3a1eb75319566741a0dda
nessy126 <nessy126@gmail.com>
2022-01-14 19:25:51 +0300

    almost finished

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,15 @@
+import Notiflix from 'notiflix';
+
 export default function fetchCountries(name) {
-  // const BASE_URL = `https://restcountries.com/v3.1/name/${name}`
-  const BASE_URL_field = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-  // const BASE_URL2 = 'https://restcountries.com/v3.1/name/peru'
-  // const BASE_URL3 = 'https://restcountries.com/v3.1/name/united'
-  return fetch(BASE_URL_field).then(response => response.json())
+  const BASE_URL = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  return fetch(BASE_URL)
+    .then(response => {
+    if (!response.ok) {
+      throw new Error(response.status)
+    }
+    return response.json()
+    })
+    .catch(error => {
+      Notiflix.Notify.failure("Oops, there is no country with that name");
+  })
 }
--- src/index.js
+++ src/index.js
@@ -6,33 +6,50 @@ import Notiflix from 'notiflix';
 const DEBOUNCE_DELAY = 300;
 const searchEl = document.querySelector('#search-box')
 const contriesList = document.querySelector('.country-list')
+const countryDiv = document.querySelector('.country-info')
 searchEl.addEventListener('input', debounce(getCountries, DEBOUNCE_DELAY))
 // fetchCountries('ukraine')
 
 function getCountries(e) {
-  fetchCountries(searchEl.value.trim()).then(data => {
+  fetchCountries(searchEl.value.trim())
+    .then(data => {
+      if (searchEl.value.trim() === '') {
+      contriesList.innerHTML = ''
+    }
+
     if (data.length > 10) {
       Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+      contriesList.innerHTML = ''
     }
-    if (data.length > 2 && data.length < 10) {
+      if (data.length >= 2 && data.length <= 10) {
+      contriesList.innerHTML = ''
       renderListMarkup(data)
     }
-    if (data.length === 1) {
+      if (data.length === 1) {
+      contriesList.innerHTML = ''
       renderCountryMarkup(data)
     }
-    // console.log(data.length)
   })
 }
 
-// function renderListMarkup(arrayOfCountries) {
+function renderListMarkup(arrayOfCountries) {
+  countryDiv.innerHTML = ''
+  const renderCountries = arrayOfCountries.map(country => {
+  return `<li class="countries"><img src="${country.flags.svg}" class="countries"></img>${country.name.official}</li>`
+}).join('')
+  contriesList.innerHTML = renderCountries
+}
 
-//   arrayOfCountries.map(coutry => {
-//   return <li></li>
-// })
-//   // contriesList
-// }
 
-
-// function renderCountryMarkup(country) {
-
-// }
+function renderCountryMarkup(countryOne) {
+  contriesList.innerHTML = ''
+  const renderCountry = countryOne.map(country => {
+    return `<li class="countries"><img src="${country.flags.svg}" class="countries"></img>${country.name.official}</li>
+    <ul class="contry-one">
+    <li class="info">Capital: <span>${country.capital}</span></li>
+    <li class="info">Population: <span>${country.population}</span></li>
+    <li class="info">Languages: <span>${Object.values(country.languages)}</span></li>
+    </ul>`
+  }).join('')
+  countryDiv.innerHTML = renderCountry
+}
